51ec012ade95a447fecbda720908b9e63e359888
gabrielSSimoura <gabriel.simourafox@gmail.com>
2021-07-09 22:36:20 -0300

    bug no while

--- main.c
+++ main.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 int main(int argc, char const *argv[])
 {
-  char *token;
+
   char *buffer;
-  size_t bufsize = 32;
-  size_t characters;
+  const char s[2] = ","; //onde havera o split;
+  size_t bufsize = 0;
+  size_t qtdChar;
 
   unsigned int k;
 
@@ -16,32 +18,33 @@ int main(int argc, char const *argv[])
   if (argc < 4)
   {
     printf("ERRO: Quantidade de argumentos inválidos\n");
-    return 0;
+    exit(1);
   }
 
   if (argc <= 1)
   {
     printf("ERRO: O diretorio de arquivos de configuracao nao foi informado\n");
-    return 0;
+    exit(1);
   }
 
   //Transforma o K (char) da leitura em unisgned int;
   k = atoi(argv[2]);
 
   // Verifica abertura de arquivos;
-  FILE *file = fopen(argv[1], 'r');
+  FILE *inputFile = fopen(argv[1], "r");
 
-  if (file == NULL)
+  if (inputFile == NULL)
   {
     printf("ERRO: falha na abertura do arquivo de entrada\n");
-    return 0;
+    exit(1);
   }
-  FILE *log = fopen(argv[3], 'w');
 
-  if (log == NULL)
+  FILE *logFile = fopen(argv[3], "w");
+
+  if (logFile == NULL)
   {
     printf("ERRO: falha na abertura do arquivo de saida\n");
-    return 0;
+    exit(1);
   }
 
   buffer = (char *)malloc(bufsize * sizeof(char));
@@ -51,9 +54,61 @@ int main(int argc, char const *argv[])
     exit(1);
   }
 
-  while (file != EOF)
+  unsigned int n = 0; //quantidade de elementos;
+  unsigned int m = 0; //quantidade de coordenadas;
+
+  char *id;
+  char *coordenada;
+  char *stringPrimeirasCoordenadas;
+
+  //Faz a leitura da primeira linha
+  qtdChar = getline(&buffer, &bufsize, inputFile);
+
+  //Le o id;
+  id = strtok(buffer, ",");
+
+  //Conta a qtd de coordenadas
+  while ((coordenada = strtok(NULL, ",")) != NULL)
   {
-    // getline(, , file);
+    m++;
+  }
+  printf("M: %d\n", m);
+
+  //Reinicia leitura do arquivo de entrada
+  rewind(inputFile);
+
+  qtdChar = getline(&buffer, &bufsize, inputFile);
+
+  double vetorCoordenadas[m];
+  unsigned int posi;
+
+  while (qtdChar > 0)
+  {
+    posi = 0;
+    n++;
+
+    //Le o id;
+    id = strtok(buffer, ",");
+
+    //Le primeira coordenada;
+    coordenada = strtok(NULL, ",");
+    while (coordenada != NULL)
+    {
+      vetorCoordenadas[posi] = atof(coordenada); //armazena a coordenada no vetor;
+      posi++;                                    // incrementa posição do vetor;
+      coordenada = strtok(NULL, ",");            //Lê a prox coordenada;
+    }
+
+    printf("%s ", id);
+    for (unsigned int i = 0; i < m; i++)
+    {
+      printf("%lf ", vetorCoordenadas[i]);
+    }
+    printf("\n");
+
+    qtdChar = getline(&buffer, &bufsize, inputFile);
+    //Cria a Struct do Ponto;
+    //segue a vida fml
   }
 
   return 0;
